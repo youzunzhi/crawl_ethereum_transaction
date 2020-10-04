e1ee577b2bfc1b783979fc057f8115d79ec57b2b
youzunzhi <youzunzhi@163.com>
2020-10-04 15:30:04 +0800

    mv if searched

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 import urllib.request as urlrequest
 import json
 import os
-import datetime
+import shutil
 import time
+import datetime
 
 apikey="F9PB22XVU41P78AVBY3RZEK8EQMXE4HAGN" #这个换成自己账户的apikey
 TXN_DF_COLUMN_NAMES = ['TxHash', 'BlockHeight', 'TimeStamp', 'From', 'To', 'Value', 'ContractAddress', 'Input', 'isError']
@@ -12,23 +13,22 @@ TXN_FIELD_NAMES = ['hash', 'blockNumber', 'timeStamp', 'from', 'to', 'value', 'c
 OUTPUT_DIR = f'results-[{(datetime.datetime.now()).strftime("%m%d%H%M%S")}]/'
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+K = 2
+node_pardir_dict = {}
+
 time_ = time.time()
 def main():
-    node_set = set()
     addresses = get_addresses()
     for address in addresses:
-        node_set = get_k_order_neighbor(node_set, OUTPUT_DIR, address, cur_order=0, k=2)
-    print_node_hash(node_set, OUTPUT_DIR)
+        get_k_order_neighbor(OUTPUT_DIR, address, cur_order=-1)
+    print_node_hash()
     # analyze_graph()
 
 
 def get_addresses():
     addresses = [
         '0x0059b14e35daB1b4EEe1e2926C7A5660dA66F747',
-        '0x008f3db10374099a11ec263415cb88c952abeedc',
-        '0x00e01A648Ff41346CDeB873182383333D2184dd1',
-        '0x020b1573f2ca670190d33ca2f0a57b0c0399ad37',
-        '0x052Ee5470868D14b158abb10DfcC76eD1cD1293d',
+        '0x7E054e0F3153caDbe040e325099a6E1d7EB4454a',
     ]
     addresses_lower = []
     for add in addresses:
@@ -36,11 +36,38 @@ def get_addresses():
     return addresses_lower
 
 
-def get_k_order_neighbor(node_set, cur_dir, address, cur_order, k):
-    if k == cur_order - 1:
+# def process_neighbor(neighbor):
+#     if neighbor in node_pardir_dict:
+#         old_order = get_order_of_searched_node(neighbor)
+#         if old_order > cur_order + 1:
+#             if old_order < K:  # which means it has its own dir
+#                 shutil.move(os.path.join(node_pardir_dict[neighbor], address), cur_dir)
+#                 node_pardir_dict[neighbor] = cur_dir
+#                 # 进入到这里说明neighbor是搜过的，应该避免重复再搜一遍。
+#             get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
+#     else:
+#         get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
+
+
+def get_k_order_neighbor(cur_dir, address, cur_order):
+    if address in node_pardir_dict and get_order_of_searched_node(address) < K:     # 搜到过且继续搜过其邻居
+        neighbors = get_neighbors(address)
+        for neighbor in neighbors:
+            if neighbor in node_pardir_dict:
+                old_order = get_order_of_searched_node(neighbor)
+                if old_order > cur_order + 1:
+                    if old_order < K:  # which means it has its own dir
+                        shutil.move(os.path.join(node_pardir_dict[neighbor], address), cur_dir)
+                        node_pardir_dict[neighbor] = cur_dir
+                        # 进入到这里说明neighbor是搜过的，应该避免重复再搜一遍。
+                    get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
+            else:
+                get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
         return
 
-    node_set.add(address)
+    node_pardir_dict[address] = cur_dir
+    if cur_order == K:
+        return
     cur_dir = os.path.join(cur_dir, address)
     os.makedirs(cur_dir, exist_ok=True)
     print(f"{cur_order}-order@{cur_dir}, {time.time()-time_:.1f}s")
@@ -51,23 +78,23 @@ def get_k_order_neighbor(node_set, cur_dir, address, cur_order, k):
     for txn in txns:
         if is_valid_txn(txn):
             txn_df = txn_df.append(txn2pdseries(txn), ignore_index=True)
-            # neighbor_from = txn['from'].lower()
-            # neighbor_to = txn['to'].lower()
-            # if neighbor_from != address:
-            #     neighbor_set.add(neighbor_from)
-            #     get_k_order_neighbor(node_set, cur_dir, neighbor_from, cur_order+1, k)
-            # elif neighbor_to != address:
-            #     neighbor_set.add(neighbor_to)
-            #     get_k_order_neighbor(node_set, cur_dir, neighbor_to, cur_order+1, k)
-            neighbor = txn['from'].lower() if txn['from'].lower()!=address else txn['to'].lower()
-            if neighbor not in cur_dir.split('/') and neighbor not in neighbor_set:
-                get_k_order_neighbor(node_set, cur_dir, neighbor, cur_order+1, k)
+            neighbor = txn['from'].lower() if txn['from'].lower() != address else txn['to'].lower()
+            if neighbor in node_pardir_dict:
+                old_order = get_order_of_searched_node(neighbor)
+                if old_order > cur_order + 1:
+                    if old_order < K:  # which means it has its own dir
+                        shutil.move(os.path.join(node_pardir_dict[neighbor], address), cur_dir)
+                        print(f"moved {os.path.join(node_pardir_dict[neighbor], address)} to {cur_dir}")
+                        node_pardir_dict[neighbor] = cur_dir
+                        # 进入到这里说明neighbor是搜过的，应该避免重复再搜一遍。
+                    get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
+            else:
+                get_k_order_neighbor(cur_dir, neighbor, cur_order + 1)
             neighbor_set.add(neighbor)
 
     txn_df.to_csv(os.path.join(cur_dir, 'txns.csv'))
     df_neighbor = pd.DataFrame(data=list(neighbor_set), columns=['address'])
     df_neighbor.to_csv(os.path.join(cur_dir, 'neighbors.csv'))
-    return node_set
 
 
 def get_txns_from_address(address):
@@ -85,9 +112,9 @@ def get_txns_from_address(address):
 
 
 def is_valid_txn(txn):
-    if txn['value'] != '0':
+    if txn['value'] == '0':
         return False
-    if txn['isError'] != '1':
+    if txn['isError'] == '1':
         return False
     if txn['from'].lower() == txn['to'].lower():
         print(txn['from'].lower())
@@ -125,20 +152,41 @@ def wei2ether(s):
     return Decimal(s1)
 
 
-def print_node_hash(node_set, output_dir):
-    print(len(node_set))
+def print_node_hash():
+    print(len(node_pardir_dict))
     HASH_LEN = 1000
     hash_dict = {}
     for i in range(HASH_LEN):
         hash_dict[i] = []
-    for node in node_set:
-        hash_idx = int(node, 16) % 100
+    for node in node_pardir_dict.keys():
+        hash_idx = int(node, 16) % HASH_LEN
         hash_dict[hash_idx].append(node)
-    with open(os.path.join(output_dir, 'node_hash.txt'), 'w') as f:
+    with open(os.path.join(OUTPUT_DIR, 'node_hash.txt'), 'w') as f:
         for i in range(HASH_LEN):
             for node in hash_dict[i]:
                 f.write(node + ' ')
             f.write('\n')
+
+
+def get_order_of_searched_node(node):
+    """
+    #node whose pardir is 'results-[1004015004]/0x0059b14e35dab1b4eee1e2926c7a5660da66f747/' is ordered 0
+    :param node:
+    :return:
+    """
+    order = len(node_pardir_dict[node].strip('/').split('/')) - len(OUTPUT_DIR.strip('/').split('/')) - 1
+    return order
+
+
+def get_neighbors(node):
+    neighbors = []
+    with open(os.path.join(node_pardir_dict[node], f'{node}/neighbors.csv'), 'r') as f:
+        l = f.readlines()
+        for line in l[1:]:
+            neighbors.append(line.split(',')[1].strip())
+    return neighbors
+
+
 
 
 if __name__ == '__main__':
