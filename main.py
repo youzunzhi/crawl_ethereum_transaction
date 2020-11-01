d4b7176b014ccc914beba5bef35de49738eb4e43
youzunzhi <youzunzhi@163.com>
2020-11-01 20:11:13 +0800

    final!

--- main.py
+++ main.py
@@ -15,24 +15,24 @@ TXN_FIELD_NAMES = ['hash', 'blockNumber', 'timeStamp', 'from', 'to', 'value', 'c
 OUTPUT_DIR = f'results-[{(datetime.datetime.now()).strftime("%m%d%H%M%S")}]/'
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-K = 1
+K = 2
 
 def get_original_nodes():
     nodes = [
         # '0xf5501ca663fee9272d71125197ebf4d250d1ce35',
         # '0x46dcc5a4f413fbf5b17f092784dbe321130c4874',
         # low risk
-        # '0xfa171c2a5BB16cD608Ce3aC7A8e8C1e4B554EcBE',
-        # '0xd1707D1696cEE3254878bd81b0aE3b7252A06B6e',
+        '0xfa171c2a5BB16cD608Ce3aC7A8e8C1e4B554EcBE',
+        '0xd1707D1696cEE3254878bd81b0aE3b7252A06B6e',
         '0x4F71D67322f7f97944c26A917acD990b793E0f2A',
-        # '0xBE38a889D67467b665E30E20eE5604A6F5696e38',
-        # '0xcDE1250f112Ac69Ae5f7D561Ad052816476Fc6d1',
+        '0xBE38a889D67467b665E30E20eE5604A6F5696e38',
+        '0xcDE1250f112Ac69Ae5f7D561Ad052816476Fc6d1',
         # high risk
-        # '0x0b7f284d74f549731499c44aed2a10adcc9e9cc0',
-        # '0xF6884686a999f5ae6c1AF03DB92BAB9c6d7DC8De',
-        # '0xDf9191889649C442836ef55De5036a7b694115b6',
-        # '0x2664c334c46635f7845487d3BAb16992Fc83A93e',
-        # '0x1f6f1723d0db4e9783b7171392b6fa9ae1062fd9',
+        '0x0b7f284d74f549731499c44aed2a10adcc9e9cc0',
+        '0xF6884686a999f5ae6c1AF03DB92BAB9c6d7DC8De',
+        '0xDf9191889649C442836ef55De5036a7b694115b6',
+        '0x2664c334c46635f7845487d3BAb16992Fc83A93e',
+        '0x1f6f1723d0db4e9783b7171392b6fa9ae1062fd9',
     ]
     nodes_lower = []
     for add in nodes:
@@ -214,7 +214,7 @@ def save_node_and_connected_component_idx(node_ordered_by_order, node_pardir_dic
         # bfs to find connected components of cur_node
         cur_queue = [cur_node]
         while cur_queue:
-            cur_queue_head = cur_queue.pop()
+            cur_queue_head = cur_queue.pop(0)
             if order_of(cur_queue_head, node_pardir_dict) == K:
                 continue
             neighbors = get_neighbors(cur_queue_head, node_pardir_dict)
